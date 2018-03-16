af3e9b667c25b5ebef14fe6a9b07a0bb0492a98a
DavidSchlo <schlosser.david.55088@gmail.com>
2018-03-16 10:32:58 -0500

    side scrolling
    Still trying to get horizontal scrolling to work...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React, { Component } from 'react';
-import HorizontalScroll from 'react-scroll-horizontal';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
-    const child = { width: '200%', height: '100%' };
+    const child = { width: `300em`, height: `100%` };
     return (
       <div className="App">
 
@@ -13,10 +12,31 @@ class App extends Component {
           <h1 className="App-title">DAVID GRAFT</h1>
         </header>
 
-        <HorizontalScroll>
-          <div style={child}>Hey there1</div>
-          <div style={child}>Hey there2</div>
-        </HorizontalScroll>
+        <div className="main">
+          <table className="main-table">
+            <tr>
+              <td className="main">
+                <div className="contentWrapper">
+                  <div className="content">
+                    sampleText tab1
+                  </div>
+                  <div className="content">
+                    sampleText tab2
+                  </div>
+                  <div className="content">
+                    sampleText tab3
+                  </div>
+                  <div className="content">
+                    sampleText tab14
+                  </div>
+                  <div className="content">
+                    sampleText tab5
+                  </div>
+                </div>
+              </td>
+            </tr>
+          </table>
+        </div>
 
         <footer className="App-footer App-border-text">
           <h1 className="App-title">DAVID GRAFT</h1>
