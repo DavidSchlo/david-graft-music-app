60fb2fcda595910c6c85599766efa8d937957eba
DavidSchlo <schlosser.david.55088@gmail.com>
2018-03-16 08:48:13 -0500

    Updates

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import HorizontalScroll from 'react-scroll-horizontal';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
+    const child = { width: '200%', height: '100%' };
     return (
       <div className="App">
 
         <header className="App-header App-border-text">
           <h1 className="App-title">DAVID GRAFT</h1>
         </header>
-        
+
+        <HorizontalScroll>
+          <div style={child}>Hey there1</div>
+          <div style={child}>Hey there2</div>
+        </HorizontalScroll>
+
         <footer className="App-footer App-border-text">
           <h1 className="App-title">DAVID GRAFT</h1>
         </footer>
